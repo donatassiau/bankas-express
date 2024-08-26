036b16a256b1ad23fa8ddef2582cfc0ed2022ef6
donatassiau <donatas.siaud@gmail.com>
2024-08-26 15:41:41 +0300

    Init

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import { pageHome } from './saskaitos/pageHome.js';
+import { pageAbout } from './saskaitos/pageAbout.js';
+import { page404 } from './saskaitos/page404.js';
 
 const app = express();
 const port = 5019;
+
+app.get('/', (req, res) => {
+    return res.send(pageHome());
+});
+
+app.get('/about', (req, res) => {
+    return res.send(pageAbout());
+});
+
+app.get('*', (req, res) => {
+    return res.send(page404());
+});
 
 app.listen(port, () => {
     console.log(`Bankas veikia: http://localhost:${port}`);   
